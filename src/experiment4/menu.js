98c7fe3f63e3be478b042e8608101d69f8bd61df
yagami <2201535398@qq.com>
2024-11-25 22:31:31 +0800

    实现了main块的变换，这个阶段花了好长时间主要是v-model太折磨了

--- src/experiment4/menu.js
+++ src/experiment4/menu.js
@@ -1,22 +1,19 @@
 const menuComponent = {
 props:["menus"],
-inherAttrs:false,
+inheritAttrs:false,
 template:`
-	<ul id="app">
+	<ul :$attrs="attrs" class="menu_ul_style">
 		<li v-for="menu in menus">
 		<font style ="cursor:pointer;" @click = "menu.show=false" v-if="menu.show">-</font>
 		<font style = "cursor:pointer;" @click = "menu.show=true" v-if ="!menu.show">+</font>
-			<a :href="menu.url">{{menu.name}}</a>
-			<ul v-show="menu.show">
+			<a :href="menu.url" @click="$emit('update:modelValue',menu.name)">{{menu.name}}</a>
+			<ul :$attrs="attrs" class="menu_ul_style" v-show="menu.show" >
 				<li v-for="submenu in menu.subMenus">
-					<a :href="submenu.url">{{submenu.name}}</a>
+					<a :href="submenu.url" @click="$emit('update:modelValue',menu.name)">{{submenu.name}}</a>
 				</li>
 			</ul>
 		</li>
 	</ul>
 	`
 	}
-
-
-
 
